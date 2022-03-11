1161447ad122eefbc9c47c26b331457a7978385f
facudelucia <facundodeluciajudo@gmail.com>
2022-03-11 14:11:58 -0300

    Start tests for GuessedWords

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,9 +1,8 @@
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import { shallow } from 'enzyme'
 import Congrats from './Congrats'
 import { findByTestAttr, checkProps } from '../test/testUtils'
 
-Enzyme.configure({ adapter: new EnzymeAdapter() })
+
 
 const defaultProps = { success: false }
 
